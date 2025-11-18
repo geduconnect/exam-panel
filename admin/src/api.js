9ed062b2df72168b2e0fd840fe7d77200ed51633
geduconnect <168544132+geduconnect@users.noreply.github.com>
2025-11-18 11:26:29 +0530

    Delete admin directory

--- admin/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000",
-  withCredentials: true, // ✅ send session cookie automatically
-});
-
-export default api;
